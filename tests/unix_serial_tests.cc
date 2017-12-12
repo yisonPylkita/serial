7e78f16e4b2ee60985c495bf0c72b4b04fd7af64
wojtek <wojciech.bartnik@polchip.pl>
2017-12-12 16:25:20 +0100

    Changed unit test framework from Google Test to Catch 2.x

--- tests/unix_serial_tests.cc
+++ tests/unix_serial_tests.cc
@@ -18,101 +18,81 @@ void loop()
 */
 
 #include <string>
-#include "gtest/gtest.h"
-
-#include <boost/bind.hpp>
-
-// Use FRIEND_TEST... its not as nasty, thats what friends are for
-// // OMG this is so nasty...
-// #define private public
-// #define protected public
-
-#include "serial/serial.h"
+#include "catch.hpp"
+#include <serial/serial.h>
 
 #if defined(__linux__)
 #include <pty.h>
+#include <unistd.h>
 #else
 #include <util.h>
 #endif
 
+
 using namespace serial;
 
-using std::string;
+class SerialFixture {
+public:
+    SerialFixture()
+    {
+        if (openpty(&master_fd, &slave_fd, name, NULL, NULL) == -1) {
+            perror("openpty");
+            exit(127);
+        }
 
-namespace {
+        REQUIRE(master_fd > 0);
+        REQUIRE(slave_fd > 0);
+        REQUIRE(std::string(name).length() > 0);
 
-class SerialTests : public ::testing::Test {
-protected:
-  virtual void SetUp() {
-    if (openpty(&master_fd, &slave_fd, name, NULL, NULL) == -1) {
-      perror("openpty");
-      exit(127);
+        port1.reset(new Serial(std::string(name), 115200, Timeout::simpleTimeout(250)));
     }
 
-    ASSERT_TRUE(master_fd > 0);
-    ASSERT_TRUE(slave_fd > 0);
-    ASSERT_TRUE(string(name).length() > 0);
+    ~SerialFixture()
+    {
+        port1->close();
+    }
 
-    port1 = new Serial(string(name), 115200, Timeout::simpleTimeout(250));
-  }
-
-  virtual void TearDown() {
-    port1->close();
-    delete port1;
-  }
-
-  Serial * port1;
-  int master_fd;
-  int slave_fd;
-  char name[100];
+protected:
+    std::unique_ptr<Serial> port1;
+    int master_fd = 0;
+    int slave_fd = 0;
+    char name[100]{};
 };
 
-TEST_F(SerialTests, readWorks) {
-  write(master_fd, "abc\n", 4);
-  string r = port1->read(4);
-  EXPECT_EQ(r, string("abc\n"));
+TEST_CASE_METHOD(SerialFixture, "read", "[serial]") {
+    write(master_fd, "abc\n", 4);
+    std::string r = port1->read(4);
+    REQUIRE(r == std::string("abc\n"));
 }
 
-TEST_F(SerialTests, writeWorks) {
-  char buf[5] = "";
-  port1->write("abc\n");
-  read(master_fd, buf, 4);
-  EXPECT_EQ(string(buf, 4), string("abc\n"));
+TEST_CASE_METHOD(SerialFixture, "write", "[serial]") {
+    char buf[5] = "";
+    port1->write("abc\n");
+    read(master_fd, buf, 4);
+    REQUIRE(std::string(buf, 4) == std::string("abc\n"));
 }
 
-TEST_F(SerialTests, timeoutWorks) {
-  // Timeout a read, returns an empty string
-  string empty = port1->read();
-  EXPECT_EQ(empty, string(""));
-  
-  // Ensure that writing/reading still works after a timeout.
-  write(master_fd, "abc\n", 4);
-  string r = port1->read(4);
-  EXPECT_EQ(r, string("abc\n"));
+TEST_CASE_METHOD(SerialFixture, "timeout", "[serial]") {
+    // Timeout a read, returns an empty string
+    std::string empty = port1->read();
+    REQUIRE(empty == std::string(""));
+
+    // Ensure that writing/reading still works after a timeout.
+    write(master_fd, "abc\n", 4);
+    std::string r = port1->read(4);
+    REQUIRE(r == std::string("abc\n"));
 }
 
-TEST_F(SerialTests, partialRead) {
-  // Write some data, but request more than was written.
-  write(master_fd, "abc\n", 4);
+TEST_CASE_METHOD(SerialFixture, "partial read", "[serial]") {
+    // Write some data, but request more than was written.
+    write(master_fd, "abc\n", 4);
 
-  // Should timeout, but return what was in the buffer.
-  string empty = port1->read(10);
-  EXPECT_EQ(empty, string("abc\n"));
-  
-  // Ensure that writing/reading still works after a timeout.
-  write(master_fd, "abc\n", 4);
-  string r = port1->read(4);
-  EXPECT_EQ(r, string("abc\n"));
-}
+    // Should timeout, but return what was in the buffer.
+    std::string empty = port1->read(10);
+    REQUIRE(empty == std::string("abc\n"));
 
-}  // namespace
-
-int main(int argc, char **argv) {
-  try {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-  } catch (std::exception &e) {
-    std::cerr << "Unhandled Exception: " << e.what() << std::endl;
-  }
-  return 1;
+    // Ensure that writing/reading still works after a timeout.
+    write(master_fd, "abc\n", 4);
+    std::string r = port1->read(4);
+    REQUIRE(r == std::string("abc\n"));
 }
--- tests/unix_timer_tests.cc
+++ tests/unix_timer_tests.cc
@@ -1,18 +1,16 @@
-#include "gtest/gtest.h"
-#include "serial/impl/unix.h"
+#include "catch.hpp"
+#include <serial/impl/unix.h>
 
 #include <unistd.h>
 #include <stdlib.h>
 
 using serial::MillisecondTimer;
 
-namespace {
-
 /**
  * Do 100 trials of timing gaps between 0 and 19 milliseconds.
  * Expect accuracy within one millisecond.
  */
-TEST(timer_tests, short_intervals) {
+TEST_CASE("short intervals", "[timer]") {
   for (int trial = 0; trial < 100; trial++)
   {
     uint32_t ms = rand() % 20;
@@ -21,11 +19,11 @@ TEST(timer_tests, short_intervals) {
     int32_t r = mt.remaining(); 
 
     // 1ms slush, for the cost of calling usleep.
-    EXPECT_NEAR(r+1, 0, 1);
+    REQUIRE((r + 1 >= 0 || r + 1 <= 1));
   }
 }
 
-TEST(timer_tests, overlapping_long_intervals) {
+TEST_CASE("overlapping long intervals", "[timer]") {
   MillisecondTimer* timers[10];
 
   // Experimentally determined. Corresponds to the extra time taken by the loops,
@@ -42,22 +40,13 @@ TEST(timer_tests, overlapping_long_intervals) {
   // Check in on them after 500ms.
   usleep(500000);
   for (int t = 0; t < 10; t++)
-  {
-    EXPECT_NEAR(timers[t]->remaining(), 500 - slush_factor + t, 5);
-  }
+    REQUIRE((timers[t]->remaining() >= 500 - slush_factor + t || timers[t]->remaining() <= 5));
 
   // Check in on them again after another 500ms and free them.
   usleep(500000);
   for (int t = 0; t < 10; t++)
   {
-    EXPECT_NEAR(timers[t]->remaining(), -slush_factor + t, 5);
+    REQUIRE((timers[t]->remaining() >= -slush_factor + t || timers[t]->remaining() <= 5));
     delete timers[t];
   }
-}
-
-}  // namespace
-
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
 }
